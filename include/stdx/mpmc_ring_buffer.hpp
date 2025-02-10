c0f7c21fec837bd3cd9a8e75bd2d21bac45cd09c
Yashar A. Rezaei <yabbarezaei@gmail.com>
2025-02-11 00:27:45 +0100

    added two helper method size and capasity. Replace a const ref push witha universal perfect forwarding variant.

--- include/stdx/mpmc_ring_buffer.hpp
+++ include/stdx/mpmc_ring_buffer.hpp
@@ -28,42 +28,40 @@ namespace stdx
         {
         }
 
+        // Delete copy constructor and copy assignment operator
+        MpmcRingBuffer(const MpmcRingBuffer&) = delete;
+        MpmcRingBuffer& operator=(const MpmcRingBuffer&) = delete;
+
+        // Delete move constructor and move assignment operator
+        MpmcRingBuffer(MpmcRingBuffer&&) = delete;
+        MpmcRingBuffer& operator=(MpmcRingBuffer&&) = delete;
+
         //--------------------------------------------------------------------------
         // push(...) : Attempt to enqueue an item (multiple producers can call this).
         // Returns true if successful, or false if the buffer is full.
         //--------------------------------------------------------------------------
-        bool push(const T &item)
+        template <typename U>
+        bool push(U &&item)
         {
-            // We use a loop with compare_exchange_weak to claim one "slot."
-            // 'head_' is the next free index for producers to write to.
-            size_t current_head = head_.load(std::memory_order_relaxed);
+            static_assert(std::is_same_v<std::decay_t<U>, T> || std::is_convertible_v<U, T>,
+                          "Pushed item must be convertible to T");
 
+            size_t current_head = head_.load(std::memory_order_relaxed);
             for (;;)
             {
-                // Acquire ensures we see the most up-to-date tail_
                 size_t current_tail = tail_.load(std::memory_order_acquire);
-
-                // If the buffer is full (distance = CAPACITY), fail immediately.
                 if (current_head - current_tail >= CAPACITY)
                 {
-                    // Buffer full
-                    return false;
+                    return false; // full
                 }
-
-                // Attempt to reserve this slot.
-                if (head_.compare_exchange_weak(
-                        current_head,
-                        current_head + 1,
-                        std::memory_order_acquire,
-                        std::memory_order_relaxed))
+                if (head_.compare_exchange_weak(current_head, current_head + 1,
+                                                std::memory_order_release,
+                                                std::memory_order_relaxed))
                 {
-                    // We have reserved slot [current_head & (CAPACITY - 1)]
-                    buffer_[current_head & (CAPACITY - 1)] = item;
+                    // Perfect-forward into the buffer
+                    buffer_[current_head & (CAPACITY - 1)] = std::forward<U>(item);
                     return true;
                 }
-
-                // If the compare_exchange_weak fails, current_head is updated;
-                // we try again in a loop.
             }
         }
 
@@ -124,13 +122,27 @@ namespace stdx
             return (head - tail >= CAPACITY);
         }
 
+        size_t size() const
+        {
+            size_t head = head_.load(std::memory_order_acquire);
+            size_t tail = tail_.load(std::memory_order_relaxed);
+            return head - tail;
+        }
+
+        size_t capacity() const
+        {
+            return buffer_.size();
+        }
+
     private:
         std::array<T, CAPACITY> buffer_;
 
         // 'head_' is how far producers have written (or reserved).
         // 'tail_' is how far consumers have read.
-        std::atomic<size_t> head_;
-        std::atomic<size_t> tail_;
+        alignas(64) std::atomic<size_t> head_;
+        char pad_[64 - sizeof(std::atomic<size_t>)];
+
+        alignas(64) std::atomic<size_t> tail_;
     };
 }
 
